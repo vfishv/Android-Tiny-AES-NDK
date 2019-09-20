a4ad4a82138a551b5b15c6aadcd80a07223c1e57
Quentin <vFISHv@gmail.com>
2019-09-21 03:55:49 +0800

    smaller

--- app/src/main/java/com/anonym/tinyaes/MainActivity.kt
+++ app/src/main/java/com/anonym/tinyaes/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.anonym.tinyaes
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
